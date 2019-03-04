02d37d0d8e47f5255fd5e2800cea593f892fe0a4
giovannidw <giovannidwidjosewojo@hotmail.com>
2019-03-04 11:01:52 +0100

    Minor changes

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 
 var camelcase = require('camelcase');
 var _ = require('lodash');
+var repeat = require('repeat-string');
+    console.log(repeat('B', 5));
+var longestStreak = require('longest-streak');
+    console.log(longestStreak('` foo `` bar `', '`')) // => 2
 
+const skinTone = require('skin-tone');
+    
 
 //console.log(camelcase('foo-bar'));
 //console.log(_('foo-bar'));
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
