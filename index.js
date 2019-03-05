a2c5ec3829c91f3bfaa3a026c5158cb5f1df9425
giovannidw <giovannidwidjosewojo@hotmail.com>
2019-03-05 12:01:10 +0100

    Gitnore update

--- index.js
+++ index.js
@@ -12,10 +12,3 @@ const skinTone = require('skin-tone');
 //console.log(camelcase('foo-bar'));
 //console.log(_('foo-bar'));
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
