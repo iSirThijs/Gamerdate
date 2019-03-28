a41e135c1f4b7f5881817c39a553cd089050fc5e
giovannidw <giovannidwidjosewojo@hotmail.com>
2019-03-29 00:26:22 +0100

    cleaned up serverjs a bit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var path = require('path');
 var bodyParser = require('body-parser')
-// var request = require('request')
 var multer = require('multer')
 var mongo = require('mongodb')
 var session = require('express-session')
@@ -15,95 +14,62 @@ mongo.MongoClient.connect(url, function (err, client) {
     db = client.db(process.env.DB_NAME)
 })
 
-
-
-
-
-
+var upload = multer({
+    dest: 'static/uploads/'
+})
 const express = require('express')
 const app = express()
 const port = 3000
 
-
-
-
-var upload = multer({
-    dest: 'static/uploads/'
-})
-
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
-
-app.use('/static', express.static('static'))
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
-
-app.get('/chat', chat)
-app.get('/', renderHome)
-
-app.get('/sign-up', renderSignup)
-app.get('/login', renderLogin)
-app.get('/profile', profile)
-app.post('/sign-up', signup)
-app.post('/login', login)
-app.post('/profile', upload.single('cover'), addGame)
-
-app.delete('/profile:id', removeGame)
-
-app.use(function (req, res, next) {
-    res.locals.user = null
-    next()
-})
-
-
-app.use(session({
-    resave: false,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET
-}))
+    app.set("view engine", "ejs")
+    app.set("views", path.join(__dirname, "views"))
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(express.static(path.join(__dirname, '/static')))
+    app.get('/chat', chat)
+    app.get('/', renderHome)
+    app.get('/sign-up', renderSignup)
+    app.get('/login', renderLogin)
+    app.get('/profile', profile)
+    app.post('/sign-up', signup)
+    app.post('/login', login)
+    app.post('/profile', upload.single('cover'), addGame)
+    app.delete('/profile:id', removeGame)
+    app.use(function (req, res, next) {
+        res.locals.user = null
+        next()
+    })
+    app.use(session({
+        resave: false,
+        saveUninitialized: true,
+        secret: process.env.SESSION_SECRET
+    }))
+    app.get('*', error)
+    app.listen(port)
 
 function renderHome(req, res) {
     res.render('pages/index')
 }
-
 function chat(req, res) {
     res.render('pages/chat')
 }
-
 function renderLogin(req, res) {
     res.render('pages/login')
 }
-
 function renderSignup(req, res) {
     res.render('pages/sign-up')
 }
-
-
 function profile(req, res, next) {
-
     db.collection('game').find().toArray(done)
-
-    function done(err, data) {
+    function done(err, game) {
         if (err) {
             next(err)
         } else {
             res.render('pages/profile', {
-                data: data
-              
-              
-
+                game: game
             })
         }
     }
 }
-
-
-
-
-
-
-
 function addGame(req, res, next) {
 // var id = slug(req.body.title).toLowerCase()
 
@@ -113,15 +79,11 @@ function addGame(req, res, next) {
     // } else {
     //     res.redirect('/sign-up')
     // }
-
-
-
     db.collection('game').insertOne({
         title: req.body.title,
         cover: req.file ? req.file.filename : null,
         description: req.body.description
     }, done)
-
     function done(err) {
         if (err) {
            next(err)
@@ -129,10 +91,7 @@ function addGame(req, res, next) {
  res.redirect('/profile')
         }
     }
- 
-
 }
-
 function removeGame(req, res, next) {
     var id = req.params.id
 
@@ -145,8 +104,6 @@ function removeGame(req, res, next) {
         _id: mongo.ObjectID(id)
     }, done)
 
-
-
     function done(err) {
         if (err) {
             next(err)
@@ -154,50 +111,26 @@ function removeGame(req, res, next) {
            res.json({status: 'ok'})
            res.redirect('/profile')
         }
-    }
-   
+    }   
 }
-
-
-
-
-
-
-
 function signup(req, res, next) {
 
-      db.collection('user').insertOne({
-          username: req.body.username,
-          password: req.body.password,
-          console: req.body.description
-      }, done)
+    db.collection('user').insertOne({
+        username: req.body.username,
+        password: req.body.password,
+        console: req.body.description
+    }, done)
 
-      function done(err) {
-          if (err) {
-              next(err)
-          } else {
-             
-               res.redirect('/profile')
-              }
-             
-              }
-
+    function done(err) {
+        if (err) {
+            next(err)
+        } else {
+            res.redirect('/profile')
+        }
+    }
 }
-
-
-
-
-      
-
-
-
-
-
 function login(req, res) {
-
     var username = req.body.username
-
-
     var password = req.body.password
 
     db.collection('user').findOne({
@@ -208,32 +141,20 @@ function login(req, res) {
 
     function done(err, user) {
         if (user) {
-            req.session.user = user
-            res.render('pages/login', {
-                id: user._id,
-                username: user.username
-            })
+                req.session.user = user
+                res.render('pages/login', {
+                    id: user._id,
+                    username: user.username
+                })
         
         } else {
             res.json({
                 status: 'ok'
-                
             })
             res.status(401).send('Password incorrect')
-            }
+        }
     } 
 }
-
-
-
-
-app.use(express.static(path.join(__dirname, '/static')))
-
-
-
-
-app.get('*', error)
-
 function error(req, res) {
     res.render('static' + req.url, function (err, html) {
         if (!err) {
@@ -247,5 +168,3 @@ function error(req, res) {
     })
 
 }
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
