61187cddb103bdd707c6bae28bb88c0933be9f3d
Thijs Spijker <thijs@tsnetwork.nl>
2019-04-05 12:35:53 +0200

    fix package.json

--- server.js
+++ server.js
@@ -3,21 +3,32 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Controllers
+const account = require('./controllers/account.js');
+const profile = require('./controllers/profile.js');
+const match = require('./controllers/match.js');
 
 server
 	.use('/static', express.static('./static'))
+	.use(bodyParser.urlencoded({extended: true}))
+	.use(session({
+		resave: false,
+		saveUninitialized: true,
+		secret: process.env.SESSION_SECRET
+	}))
 	.set('view engine', 'ejs')
 	.set('views', './views')
-	.use(bodyParser.urlencoded({extended: true}))
-	// .use(session({
-	// 	resave: false,
-	// 	saveUninitialized: true,
-	// 	secret: process.env.SESSION_SECRET
-	// }))
+	.use(setLocals)
 	.get('/', (req, res) => res.render('index.ejs'))
+	.use('/account', account)
+	.use('/profile', profile)
+	.use('/match', match)
 	.use(notFound)
 	.use(errorHandler)
 	.listen(process.env.PORT || 8000);
+
 
 function notFound(req, res) {
 	res.locals.code = 404;
@@ -28,4 +39,14 @@ function notFound(req, res) {
 function errorHandler(err, req, res) {
 	res.locals.code = 500;
 	res.status(500).res.render('error-page.ejs');
+}
+
+function setLocals(req, res, next) {
+	if (req.session.user) {
+		res.locals.user = req.session.user;
+		next();
+	} else {
+		res.locals.user = false;
+		next();
+	}
 }
