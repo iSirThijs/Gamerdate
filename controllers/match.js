f420cd72d8f736e73ca819c4f8fb782d2c974572
Kuckelkorn <remco.kuckelkorn@gmail.com>
2019-04-10 22:05:57 +0200

    update userschema to include no match

--- controllers/match.js
+++ controllers/match.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const mongoose = require('mongoose');
 require('dotenv').config();
 const User = require('../model/user');
+//const Game = require('../model/game');
 
 router
-	.get('/', matchPage)
-	.post('/:id', addMatch);
+	.get('/', matchPage) //eslint-disable-line
+	//.post('/:id', addMatch);
 
 async function matchPage(req, res) {
-	//let id = req.session.user.id;
-	const userLoggedIn = res.local.user;
-	let matches = await findMatches(userLoggedIn);
+	let id = req.session.user.id;
+	let matches = await findMatches(id);
 	let noDups = await rmDuplicates(matches);
-	//console.log(noDups); //eslint-disable-line
 	res.render(('matches.ejs'), {
 		noDups
 	});
 }
 
-function findMatches(userLoggedIn) {
+function findMatches(id) {
 	return new Promise(function(resolve) {
 		mongoose.connect(process.env.MONGO_DB, {
 			useNewUrlParser: true,
@@ -29,29 +28,28 @@ function findMatches(userLoggedIn) {
 		db.on('error', (err, next) => next(err));
 		db.once('open', async function() {
 			// Find your own data
-			let data = await User.findOne({
-				username: userLoggedIn
-			});
+			let data = await User.findById(id);
 			// Find the games in your game library and match it to others
 			let matches = [];
 			let userGames = data.games;
-			let confirmedMatch = data.match;
+			let prevMatched = data.match;
+			let matchDenied = data.noMatch;
 			for (let i = 0; i < userGames.length; i++) {
 				let game = userGames[i];
 				matches.push(await User.find({
 					$and: [{
 						games: game
 					}, {
-						username: {
-							$ne: userLoggedIn,
-							$nin: confirmedMatch
+						_id: {
+							$ne: id,
+							$nin: (prevMatched, matchDenied)
 						}
 					}]
-				}));
+				}).populate('games'));
 			}
+			console.log(userGames); //eslint-disable-line
 			let flatMatch = matches.flat();
 			resolve(flatMatch);
-			console.log(userGames) //eslint-disable-line
 		});
 	});
 }
@@ -75,16 +73,28 @@ function rmDuplicates(arr) {
 	});
 }
 
-async function addMatch(req, res) {
-	const userLoggedIn = res.locals.user;
-	await User.update({
-		username: userLoggedIn
-	}, {
-		$push: {
-			match: req.params.id
-		}
-	});
-	res.redirect('/match');
-}
+// async function addMatch(req, res, next) {
+// 	const value = req.body.match;
+// 	const user = await User.findById(req.session.user.id);
+//
+// 	if (value === 'positive') {
+// 		try {
+// 			user.match.push(req.params.id);
+// 			await user.save();
+// 			res.redirect('/match');
+// 		} catch (err) {
+// 			next(err);
+// 		}
+// 	} else if (value === 'negative') {
+// 		try {
+// 			user.noMatch.push(req.params.id);
+// 			await user.save();
+// 			res.redirect('/match');
+// 		} catch (err) {
+// 			next(err);
+// 		}
+// 	}
+// }
+
 
 module.exports = router;
--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const userSchema = new mongoose.Schema({
 	name: String,
 	lastname: String,
@@ -7,11 +8,13 @@ const userSchema = new mongoose.Schema({
 	username: String,
 	hash: String,
 	gender: String,
-	//games: [{type: mongoose.Schema.Types.Number, ref:'Game'}],
-	games:Array,
-	match: Array
+	games: [{type: mongoose.Schema.Types.Number, ref:'Game'}],
+	match: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+	noMatch: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}]
+	// games: Array
 });
 
-const User = mongoose.model('User', userSchema);
+
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
