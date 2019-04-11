05a6e871875519bbd8ff2e48d99ddd9611e4f168
emmaoudmaijer <emma.oudmaijer@hva.nl>
2019-04-11 15:19:24 +0200

    add form slider

--- static/scripts/account.js
+++ static/scripts/account.js
@@ -19,24 +19,29 @@ function GetFieldSet(){
 	let buttonClicked = event.target.id;
 	let sliderDirection = buttonClicked.substr(-5,4);
 	let sliderLevel = buttonClicked.substr(-1,1);
+	let form = document.getElementById('register-slider');
 
 	if(sliderDirection == 'Next'){
 		if(sliderLevel == 1){ 
-			document.getElementById('fieldset1id').style.visibility = 'hidden';
-			document.getElementById('fieldset2id').style.visibility = 'visible';
+			// document.getElementById('fieldset1id').style.visibility = 'hidden';
+			form.style.transform = 'translate(-100%)';
+			// document.getElementById('fieldset2id').style.visibility = 'visible';
 		}
 		if(sliderLevel == 2){ 
-			document.getElementById('fieldset2id').style.visibility = 'hidden';
-			document.getElementById('fieldset3id').style.visibility = 'visible';	
+			// document.getElementById('fieldset2id').style.visibility = 'hidden';
+			form.style.transform = 'translate(-202%)';
+			// document.getElementById('fieldset3id').style.visibility = 'visible';	
 		}
 	} else {
 		if(sliderLevel == 2){ 
-			document.getElementById('fieldset1id').style.visibility = 'visible';
-			document.getElementById('fieldset2id').style.visibility = 'hidden';
+			// document.getElementById('fieldset1id').style.visibility = 'visible';
+			form.style.transform = 'translate(0%)';
+			// document.getElementById('fieldset2id').style.visibility = 'hidden';
 		}
 		if(sliderLevel == 3){ 
-			document.getElementById('fieldset2id').style.visibility = 'visible';
-			document.getElementById('fieldset3id').style.visibility = 'hidden';	
+			// document.getElementById('fieldset2id').style.visibility = 'visible';
+			form.style.transform = 'translate(-100%)';
+			// document.getElementById('fieldset3id').style.visibility = 'hidden';	
 		}
 	}
 
