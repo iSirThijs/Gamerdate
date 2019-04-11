5dee4300b326d0bd425c0bfb822b678a148955ed
Thijs Spijker <thijs@tsnetwork.nl>
2019-04-11 13:53:58 +0200

    Revert "Revert "Merge branch 'development' into accounts""
    This reverts commit e9450d837d94e260c7f66967c99f15bf5ca1f79f.

--- server.js
+++ server.js
@@ -24,10 +24,12 @@ server
 	.set('view engine', 'ejs')
 	.set('views', './views')
 	.use(setLocals)
+
 	.get('/', (req, res) => res.render('index.ejs'))
 	.use('/account', account)
 	.use('/profile', loginUtil.require, profile)
 	// .use('/match', match)
+
 	.use(notFound)
 	.use(errorHandler)
 	.listen(process.env.PORT || 8000);
@@ -35,18 +37,26 @@ server
 
 function notFound(req, res) {
 	res.locals.code = 404;
-	res.locals.message = 'Not found';
+	res.locals.title = 'Not found';
 	res.status(404).render('error-page.ejs');
 }
 
-function errorHandler(err, req, res) {
+
+function errorHandler(err, req, res, next) {
 	res.locals.code = 500;
-	res.status(500).res.render('error-page.ejs');
+	res.locals.title = 'Server Error';
+	res.locals.message = err.message;
+	res.status(500).render('error-page.ejs');
+	console.log('ErrorHandler' + '\n' + err); //eslint-disable-line
+	next();
 }
 
 function setLocals(req, res, next) {
-	if (req.session.user) {
-		res.locals.user = req.session.user;
+	if (req.session.user)  {
+		res.locals.user = {
+			username: req.session.user.username,
+			id: req.session.user.id
+		};
 		next();
 	} else {
 		res.locals.user = false;
